b9093f187c5d41e97dcf2e55a216cd77f8a2e8a6
Dhruv Malhotra <dhruvmalhotra2911@gmail.com>
2026-04-26 18:21:34 +0530

    Added Production URL to CORS ALLOWED ORIGINS

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -28,9 +28,9 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS","localhost,127.0.0.1").split(",")
 #ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # React dev port
-]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS","http://localhost:5173").split(",")
+  # React dev port
+
 
 # Application definition
 
