c67de36a93d5d214115a14829101346fffaca6f8
Dhruv Malhotra <dhruvmalhotra2911@gmail.com>
2026-05-11 16:05:35 +0530

    fix: auth interceptor to avoid session expiry toast on login failure

--- react-frontend/src/api/axiosInstance.js
+++ react-frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import { getToken } from "../utils/auth";
 import { toast } from "react-toastify";
 
 const api = axios.create({
- //   baseURL: "http://127.0.0.1:8000/api"
+//    baseURL: "http://127.0.0.1:8000/api"
       baseURL: import.meta.env.VITE_API_URL
 });
 
@@ -19,7 +19,11 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        if(error.response && error.response.status === 401){
+        if(
+            error.response && 
+            error.response.status === 401 &&
+            !error.config.url.includes("/login")
+        ) {
 
             localStorage.removeItem("token");
 
